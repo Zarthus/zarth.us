0ad091fd1f80d9b89f2ff3f201f24bd4ff2375ab
Zarthus <BuuGhost@gmail.com>
2014-03-20 13:22:20 +0100

    The Head is now an own includable file on its own rather than a function in functions.php

--- zarth.us/includes/php/functions.php
+++ zarth.us/includes/php/functions.php
@@ -78,56 +78,6 @@ function getDomainName($markup = false)
 	}
 }
 
-function parseDefaultHead($web)
-{
-	$extras = "";
-	$theme = "";
-	
-	if (!empty($web['description']))
-	{
-		$desc = addslashes($web['description']);
-		$extras .= "<meta name=\"description\" content=\"$desc\">\n";
-	}
-
-	if (!empty($web['theme']) && file_exists('./includes/css/' . $web['theme']))
-	{
-		$theme .= "<link rel='stylesheet' href='./includes/css/" . $web['theme'] . "'>";
-	}
-	else
-	{
-		$theme .= '<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">';
-	}
-	
-	$title = '';
-	$title .= !empty($web['title_prefix']) ? $web['title_prefix'] . ' | ' : '';
-	$title .= !empty($web['title']) ? $web['title'] : getSiteName(); 
-	$title .= !empty($web['title_suffix']) ? ' | ' . $web['title_suffix'] : '';
-	
-	echo <<<HEAD
-
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	{$extras}
-
-	<title>{$title}</title>
-	
-	<link rel="shortcut icon" type="image/png" href="favicon.png">
-
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	{$theme}
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
-
-
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-HEAD;
-}
-
 function parseDefaultBody($ga = array())
 {
 	$ganal = getGoogleAnalytics($ga);
