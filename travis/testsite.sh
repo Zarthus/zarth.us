e113b74a81a3e5c72b66f83963c673ca7075b3fe
Zarthus <zarthus@zarth.us>
2014-06-11 09:44:32 +0200

    Travis attempt number #9001

--- travis/testsite.sh
+++ travis/testsite.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mv ./travis/config.default.php ./zarth.us/includes/php/config.php
-mv ./travis/zarth.us.php ./zarth.us/zarth.us.php
+mv travis/config.default.php zarth.us/includes/php/config.php
+mv travis/zarth.us.php zarth.us/zarth.us.php
 
-chmod +x ./zarth.us/zarth.us.php
+chmod +x zarth.us/zarth.us.php
 
-EXIT_CODE=php ./zarth.us/zarth.us.php
+EXIT_CODE=php zarth.us/zarth.us.php > /dev/null
 
 if [ "$EXIT_CODE" != 0 ]; then 
 	exit 1
