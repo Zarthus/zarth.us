14b8828a6e0b4e5de850150db91c1f6267e98936
Zarthus <zarthus@zarth.us>
2014-06-11 09:55:49 +0200

    Try making the database via .travis.yml

--- travis/makedb.sh
+++ travis/makedb.sh
@@ -2,7 +2,6 @@
 
 DATABASE_NAME=zarthus
 
-mysql -e 'CREATE DATABASE IF NOT EXISTS `$DATABASE_NAME`;'
 mysql $DATABASE_NAME < database/logs.sql
 mysql $DATABASE_NAME < database/projects.sql
 mysql $DATABASE_NAME < database/visitor.sql
